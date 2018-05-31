15c147b21b6e73e60da38046a8709fce1788e175
Chris Rusu <33431729+ChrisRusu1@users.noreply.github.com>
2018-05-31 18:26:17 -0400

    more

--- same/Print.cpp
+++ same/Print.cpp
@@ -4,20 +4,23 @@ using namespace std;
 
 int main ()
 {
-	int f = 0;
-  string str {"abcdefg"};
+	int r = 0;
+	int f;
+	string str = "";
+	cout << "what do you want to see ";
+	getline(cin, str);
+	cout << "how many letters do you want to show ";
+	cin >> f;
+
   for (char c : str)
   {
-  f++;
+
+	  r++;
   cout << "[" << c << "]";
   cout << '\n';
-if (f == 4){
+if (r == f){
 	break;
 }
 	}
-
-
-
-
  return 0;
  }
